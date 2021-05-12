b2c45fc2dd13e61b1906996ed6578a941bb924d4
Salih Saygi <salih.saygie@gmail.com>
2021-05-12 14:52:55 -0700

    sample data for testing

---
+++ test/data/bot/testbots.js
@@ -0,0 +1,25 @@
+const users = [
+  {
+    githubId: 'CanMenderes',
+    displayName: 'Can Menderes',
+    image: 'https://github.com/identicons/app/oauth_app/1596867',
+    email: 'canmenderes@example.com',
+    hasBots: ['TestUser1', 'MusicPlayer3']
+  },
+  {
+    name: 'JohnDoe',
+    displayName: 'John Doe',
+    image: 'https://github.com/identicons/app/oauth_app/1596867',
+    email: 'johndoe@example.com',
+    hasBots: ['Bot2', 'ChatFilter']
+  },
+  {
+    name: 'JaneSnow',
+    displayName: 'Jane Snow',
+    image: 'https://github.com/identicons/app/oauth_app/1596867',
+    email: 'janesnow@example.com',
+    hasBots: ['ForrestGump', "Father"]
+  },
+]
+
+export default users
