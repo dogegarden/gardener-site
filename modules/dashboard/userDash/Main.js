9ce7c154d950da192290521796bfdeeb2b173975
Salih Saygi <salih.saygie@gmail.com>
2021-05-12 14:48:44 -0700

    Dashboard with Sidebar and Dropdown

---
+++ modules/dashboard/userDash/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Dropdown from '../Dropdown/Main';
+import Sidebar from '../Sidebar/Main';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Dropdown />
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Dashboard;
